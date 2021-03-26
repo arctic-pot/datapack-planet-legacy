34d4f76596b200f75b9675d75591f5d399329175
TheColdPot <guohw979@outlook.com>
2021-03-26 19:57:41 +0800

    chore: for bugs shelve incomplete field callout

--- src/App.tsx
+++ src/App.tsx
@@ -21,23 +21,8 @@ function App(): JSX.Element {
   // This array stores namespace Id
   const [openingTabs, setOpeningTabs] = useState<Array<string>>([]);
   const [, setSelectingTab] = useState<number>(0);
-  const [calloutsShow, _setCalloutsShow] = useState(true);
-
-  setTimeout(() => {
-    _setCalloutsShow(false);
-  }, 5 * 1000);
-
   return (
     <IntlProvider messages={messages} locale={locale}>
-      {sessionStorage.getItem('missingMessages') ? (
-        <Callout hidden={!calloutsShow}>
-          <div style={{ padding: 10 }}>
-            Your selected language does not completed.
-            <br />
-            We replaced incomplete fields into default language
-          </div>
-        </Callout>
-      ) : null}
       <CommandsBar
         openingTabsState={[openingTabs, setOpeningTabs]}
         selectingTabState={[null, setSelectingTab]}
