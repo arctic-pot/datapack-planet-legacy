2a8e153dcb85f5b49e349696312a122c03746118
TheColdPot <guohw979@outlook.com>
2021-04-10 15:11:11 +0800

    chore: update webpack settings

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,6 +4,9 @@ const plugins = require('./webpack.plugins');
 rules.push({
   test: /\.s?css$/,
   use: [{ loader: 'style-loader'}, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+}, {
+  test: /\.txt/,
+  type: 'asset'
 });
 
 module.exports = {
