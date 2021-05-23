f22cfa942185dd525a640f61b5dff9eb434a7e36
TheColdPot <guohw979@outlook.com>
2021-05-23 19:46:17 +0800

    chore: add basic unit test

---
+++ test/spec.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const Application = require('spectron').Application;
+const electronPath = require('electron');
+const path = require('path');
+const expect = require('chai').expect;
+
+describe('Application Launch', function () {
+  this.timeout(10000);
+  let app;
+
+  beforeEach(function () {
+    app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '..')]
+    });
+    return app.start();
+  });
+
+  afterEach(function () {
+    if (app && app.isRunning()) {
+      return app.stop()
+    }
+  });
+
+  it('shows an initial window', function () {
+    return app.client.getWindowCount().then(function (count) {
+      expect(count).to.be.deep.equal(1)
+    });
+  });
+});
